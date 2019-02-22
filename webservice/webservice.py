510c2791571144d7e431d68fbbbbc694e0eb03d6
Jonathan Evertz <jonathan@jonathan-thinkpad>
2019-02-22 13:25:25 +0100

    test

--- webservice/webservice.py
+++ webservice/webservice.py
@@ -1,6 +1,16 @@
+#!/usr/bin/python3
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/test')
+def test():
+    return 'test uff!'
+
+if __name__ == '__main__':
+    app.run()
