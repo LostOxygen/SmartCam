1477fe529a6e540fa90132004503b1120b85f2e0
Jonathan Evertz <jonathan@jonathan-thinkpad>
2019-03-22 13:25:26 +0100

    fix

--- tcp_server/echo-server.py
+++ tcp_server/echo-server.py
@@ -8,25 +8,24 @@ PORT = 65432 #Port auf dem gehört wird
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: #AF_INET = Inet Adress Family (IPv4), SOCK_STREAM = socket type (TCP)
     sock.bind((HOST,PORT))
     sock.listen()
-
-    with conn:
-
-        while True:
-            conn, addr = sock.accept()
+    while True:
+        conn, addr = sock.accept()
+        with conn:
             print('Connected by: ', addr)
-            data = conn.recv(1024) #empfängt Daten der Verbindung (max 1024 Byte)
-            data = data.decode() #Dekodiert, weil Binär
-            if not data:
-                #print('Warte auf Daten..')
-                pass
+            while True:
+                data = conn.recv(1024) #empfängt Daten der Verbindung (max 1024 Byte)
+                data = data.decode() #Dekodiert, weil Binär
+                if not data:
+                    #print('Warte auf Daten..')
+                    pass
 
-            if str(data) == 'test':
-                ausgabe1 = 'Hallo'
-                ausgabe1 = ausgabe1.encode() #wieder in binär kodieren
-                conn.sendall(ausgabe1)
+                if str(data) == 'test':
+                    ausgabe1 = 'Hallo'
+                    ausgabe1 = ausgabe1.encode() #wieder in binär kodieren
+                    conn.sendall(ausgabe1)
 
-            if str(data) == 'exit':
-                ausgabe2 = 'Ciao'
-                ausgabe2 = ausgabe2.encode()
-                conn.sendall(ausgabe2)
-                break
+                if str(data) == 'exit':
+                    ausgabe2 = 'Ciao'
+                    ausgabe2 = ausgabe2.encode()
+                    conn.sendall(ausgabe2)
+                    break
