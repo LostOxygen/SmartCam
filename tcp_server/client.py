925290497f05c443017baa64852d188bd2e3693d
Jonathan Evertz <jonathan@jonathan-thinkpad>
2019-03-26 11:17:23 +0100

    Überarbeitung

--- tcp_server/client.py
+++ tcp_server/client.py
@@ -7,14 +7,16 @@ PORT = 65432 #Port des Servers
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST,PORT))
+    print("Befehle: \n EX : beendet Server und Verbindung \n GO : liefert Offset zurueck \n CO : konfiguriert die Kreiserkennung")
     while True:
         command = input("Befehl: ")
         command = command.encode()
         sock.sendall(command)
         data = sock.recv(1024)
-        #response = repr(data)
         response = data.decode()
-        print('Received Data: ', response)
-        if str(response) == 'Ciao':
+        print('Empfangene Daten: ', response)
+
+        if response[0] == 'E' and response[1] == 'X':
+            print("Verbindung wird beendet..")
             break
 print("Verbindung Beendet!")
