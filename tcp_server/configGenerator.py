914bc6033008949ceca6d8ffa2d5d83df15f873e
Jonathan Evertz <jonathan@jonathan-thinkpad>
2019-04-08 10:42:00 +0200

    config camera adj.

--- tcp_server/configGenerator.py
+++ tcp_server/configGenerator.py
@@ -2,7 +2,7 @@
 
 ##########################
 # Author: Jonathan Evertz
-# Date: 07.03.2019
+# Date: 04.08.2019
 # Generator für OpenCV Config File
 ##########################
 
@@ -13,19 +13,9 @@ import numpy as np
 import argparse
 import math
 import configparser
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
-
 
 class Config():
-    def createConfig():
-        cam = PiCamera()
-        cam.resolution = (1920, 1088)
-        cam.framerate = 30
-        rawCapture = PiRGBArray(cam, size=(1920, 1088))
-        time.sleep(1) #eine Sekunde warten, damit die Kamera fokussieren kann
-
+    def createConfig(camera):
         #Variablen
         kreis_durchmesser_mm = 7
         min_threshold = 0
@@ -39,33 +29,31 @@ class Config():
         config = configparser.ConfigParser()
 
         # ----------------------------------- Main Code -----------------------
-        cam = cv2.VideoCapture(0)
-        while cam.isOpened():
-            ret, frame = cam.read()
-            # in Graubild umwandeln
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            #blurren
-            blur = cv2.medianBlur(gray, 5)
-            #zeichnet Rechteckt
-            rechteck = cv2.rectangle(frame, oben_links, unten_rechts, (100,50,200), 5)
-            #Temp Variablen zum arbeiten für nächsten Kreis
-            kdistanz = 100000
-            kkreis_r = 0
-            kkreis_xy = (0,0)
-            #verarbeitet den Teil im Rechteck
-            ausschnitt = blur[oben_links[1] : unten_rechts[1], oben_links[0] : unten_rechts[0]]
-            mittelpunkt = (int(oben_links[0]+(unten_rechts[0]-oben_links[0])/2), int(oben_links[1]+(unten_rechts[1]-oben_links[1])/2))
-            cv2.circle(ausschnitt,mittelpunkt,2,(0,0,255),3)
-            circles = cv2.HoughCircles(ausschnitt,cv2.HOUGH_GRADIENT,1,20,param1=100,param2=20,minRadius=27,maxRadius= 32)
-            if circles is not None:
-                for i in circles[0,:]:
-                    kkreis_r = i[2]
-                    kkreis_xy = (int(oben_links[0] + i[0]), int(oben_links[1] + i[1]))
+        ret, frame = camera.get_frame_cv()
+        # in Graubild umwandeln
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #blurren
+        blur = cv2.medianBlur(gray, 5)
+        #zeichnet Rechteckt
+        rechteck = cv2.rectangle(frame, oben_links, unten_rechts, (100,50,200), 5)
+        #Temp Variablen zum arbeiten für nächsten Kreis
+        kdistanz = 100000
+        kkreis_r = 0
+        kkreis_xy = (0,0)
+        #verarbeitet den Teil im Rechteck
+        ausschnitt = blur[oben_links[1] : unten_rechts[1], oben_links[0] : unten_rechts[0]]
+        mittelpunkt = (int(oben_links[0]+(unten_rechts[0]-oben_links[0])/2), int(oben_links[1]+(unten_rechts[1]-oben_links[1])/2))
+        cv2.circle(ausschnitt,mittelpunkt,2,(0,0,255),3)
+        circles = cv2.HoughCircles(ausschnitt,cv2.HOUGH_GRADIENT,1,20,param1=100,param2=20,minRadius=27,maxRadius= 32)
+        if circles is not None:
+            for i in circles[0,:]:
+                kkreis_r = i[2]
+                kkreis_xy = (int(oben_links[0] + i[0]), int(oben_links[1] + i[1]))
 
-            config['KREISERKENNUNG'] = {'AbstandZumObjekt' : '15', 'DurchmesserKreisInPixel' : kkreis_r}
-            with open('config.ini', 'w') as configfile: #Werte in Config schreiben
-                config.write(configfile)
+        config['KREISERKENNUNG'] = {'AbstandZumObjekt' : '15', 'DurchmesserKreisInPixel' : kkreis_r}
+        with open('config.ini', 'w') as configfile: #Werte in Config schreiben
+            config.write(configfile)
 
-            break
+        break
 
-        return True
+    return True
