a273fb2f2e1ff8fb564f265eaa54bd0f303c404e
Jonathan Evertz <jonathan@jonathan-thinkpad>
2019-04-12 12:38:15 +0200

    fix

--- tcp_server/server.py
+++ tcp_server/server.py
@@ -154,5 +154,5 @@ def main():
 
 
 if __name__ == '__main__':
-    strip = NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_STRIP)
+    strip = NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
     main()
