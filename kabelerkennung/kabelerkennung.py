05f56453e7ce965f1740e0946f2997fb3a4b9108
Jonathan Evertz <jonathan@jonathan-thinkpad>
2019-05-03 11:20:00 +0200

    zeichnet nur relevante Punkte

--- kabelerkennung/kabelerkennung.py
+++ kabelerkennung/kabelerkennung.py
@@ -21,6 +21,9 @@ threshold_val = 100
 threshold_max = 300
 oben_links = (400,150)
 unten_rechts = (900,600)
+maxCorners = 300 #Anzahl zu erkennenden Kanten
+qualityLevel = 0.03 #je höher desto genauer
+minDistance = 10 #mindeste Distanz zwischen Punkten
 
 # ----------- Config einlesen und überprüfen --------------------------
 config = configparser.ConfigParser()
@@ -59,28 +62,33 @@ def main(argv):
     #ret, thresh = cv2.threshold(blur, threshold_val, threshold_max, cv2.THRESH_BINARY) #Threshold generieren
     contours, hierarchy = cv2.findContours(blur, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #Konturen suchen
 
-    corners = cv2.goodFeaturesToTrack(gray, 300, 0.01, 10)
+    corners = cv2.goodFeaturesToTrack(gray, maxCorners, qualityLevel, minDistance)
     #corners = np.int0(corners)
 
     min_xy = (1000,1000) #temp Var zum finden von punkt ganz oben_links
-    for i in corners:
-        x,y = i.ravel()
-        cv2.circle(img, (x,y), 2, (0,0,255), 2)
-        if x < min_xy[0]: #guckt nach kleinstem x wert
-            min_xy = (x,y)
+    if corners is not None:
+        for i in corners:
+            x,y = i.ravel()
+            if x <= 1100: #zeichnet nur Relevante Punkte
+                cv2.circle(img, (x,y), 2, (0,0,255), 2)
+            if x < min_xy[0]: #guckt nach kleinstem x wert
+                min_xy = (x,y)
 
+    #berechnet Distanz
+    dist = math.sqrt((min_xy[0] - min_xy[0])**2 + (min_xy[1] - mittelpunkt[1])**2)
+
+#----------- optische Ausgabe --------------------------
     cv2.circle(img, min_xy, 2, (0,255,0), 2) #zeichnet punkt ganz links
     cv2.line(img, min_xy, (min_xy[0], mittelpunkt[1]), (255,255,0), 2) #zeichnet linie von punkt nach oben
     #zeichnet Mittelpunkt und Linie nach links
     cv2.circle(img, mittelpunkt, 2, (255,255,0), 2)
     cv2.line(img, mittelpunkt, (min_xy[0],mittelpunkt[1]), (255,255,0), 2)
     cv2.line(img, mittelpunkt, min_xy, (255,255,0), 2)
-
-    dist = math.sqrt((min_xy[0] - min_xy[0])**2 + (min_xy[1] - mittelpunkt[1])**2)
+    #erzeugt Text mit Pixelangabe
     cv2.putText(img, str(round(dist, 2)) + "px diff." , (min_xy[0],mittelpunkt[1]), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 1, cv2.LINE_AA, 0)
-
     #Konturen zeichnen
     cv2.drawContours(img, contours, -1, (0,255,0), 3)
+#-------------------------------------------------------
 
     cv2.namedWindow(fenster_name, 1)
     cv2.imshow(fenster_name, img)
@@ -89,7 +97,7 @@ def main(argv):
 
     print("Distanz: " + str(round(umrechnung_pixel_mm * dist)) + "mm")
     #cv2.imwrite("bild.jpg", img) #speichert ein Bild
-    #plt.imshow(img), plt.show()
+    plt.imshow(img), plt.show()
 
 
 if __name__ == "__main__":
