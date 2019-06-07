d0f21ffd19add68e4d436ee17314991146e2121b
Jonathan Evertz <jonathan.evertz@gmail.com>
2019-06-07 12:57:02 +0200

    changed command list

--- raspberry_pi/tcp_server/server.py
+++ raspberry_pi/tcp_server/server.py
@@ -55,7 +55,7 @@ def make_picture(camera, fileName): #Funktion zum Bild erstellen
 
 #---------------------------- Beginn Server ------------------------------------------------------
 def main():
-    print("Befehle: \n EX : beendet Server und Verbindung \n GO : liefert Offset zurueck \n CO : konfiguriert die Kreiserkennung \n IM : erstellt ein einfaches Bild \n CV : erstellt ein Bild mit erkannten Kreisen \n FX123: setzt LED-Ring auf Farbwert 123,123,123 \n KO: liefert Kabeloffset zurück \n")
+    print("Befehle: \n EX : beendet Server und Verbindung \n GO : liefert Offset zurueck \n CFGx : konfiguriert die Kreiserkennung \n IM : erstellt ein einfaches Bild \n CV : erstellt ein Bild mit erkannten Kreisen \n FX123: setzt LED-Ring auf Farbwert 123,123,123 \n KOx: liefert Kabeloffset zurück \n")
     #strip.begin()
     print("Hostet Server auf: " + HOST + " auf Port: " + str(PORT))
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: #AF_INET = Inet Adress Family (IPv4), SOCK_STREAM = socket type (TCP)
