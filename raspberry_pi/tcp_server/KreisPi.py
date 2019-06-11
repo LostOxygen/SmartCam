928e5df72b0ae1691d1cb35cb71bb42f26dbc900
Jonathan Evertz <jonathan.evertz@gmail.com>
2019-06-11 12:43:16 +0200

    config fix

--- raspberry_pi/tcp_server/KreisPi.py
+++ raspberry_pi/tcp_server/KreisPi.py
@@ -53,13 +53,13 @@ class Kreis():
             print('Config konnte nicht gefunden werden. Bitte erst mit configGenerator.py eine Config generieren lassen!')
             config_test = False
 
-        kreis_durchmesser_pixel = float(config['CONFIG']['durchmesserkreisinpixel']) #fragt Wert aus Config File ab
+        durchmesser_pixel = float(config['CONFIG']['mm_pro_pixel1']) #fragt Wert aus Config File ab
 
-        if kreis_durchmesser_pixel == 0:
-            kreis_durchmesser_pixel = 1
+        if durchmesser_pixel == 0:
+            durchmesser_pixel = 1
             print("kreis_durchmesser_pixel war 0 und wurde auf 1 gesetzt")
 
-        umrechnung_pixel_mm = kreis_durchmesser_mm / kreis_durchmesser_pixel #Rechnet mm pro Pixel aus
+        umrechnung_pixel_mm = kreis_durchmesser_mm / durchmesser_pixel #Rechnet mm pro Pixel aus
 
         # ----------------------------------- Main Code -----------------------
         if config_test:
