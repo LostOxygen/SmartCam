ef00b7953b447120c261da7ad7eb6fd49b228876
Jonathan Evertz <jonathan.evertz@gmail.com>
2019-07-24 16:54:02 +0200

    test script for edge detection

---
+++ non_raspberry_pi/quadratconfig/test.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+filename = "bild1.jpg"
+img = cv2.imread(filename)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+
+dst = cv2.dilate(dst, None)
+print(dst.max())
+img[dst > 0.01 * dst.max()] = [0, 0, 255]
+
+print(dst[0])
+
+cv2.imshow('dst', img)
+
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows()
