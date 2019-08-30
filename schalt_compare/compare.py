3aad54db4843ee9543df24eb7897d1500268f971
jonathan <jonathan.evertz@gmail.com>
2019-08-30 14:37:11 +0200

    init compare

---
+++ schalt_compare/compare.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+
+import cv2
+import sys
+import os
+import numpy as np
+import argparse
+import math
+import configparser
+import time
+from PIL import Image
+import io
+from pathlib import Path
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from datetime import datetime
+import socket
+import fcntl
+import struct
+import csv
