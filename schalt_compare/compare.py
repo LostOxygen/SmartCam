ed81c132c77ce99e707fdb73b349e3528bc7dc20
jonathan <jonathan.evertz@gmail.com>
2019-09-06 09:29:50 +0200

    first comparison

--- schalt_compare/compare.py
+++ schalt_compare/compare.py
@@ -1,21 +1,55 @@
-#!/usr/bin/python3
-# -*- coding: utf8 -*-
-
-import cv2
-import sys
-import os
 import numpy as np
-import argparse
-import math
-import configparser
-import time
-from PIL import Image
-import io
-from pathlib import Path
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-from datetime import datetime
-import socket
-import fcntl
-import struct
-import csv
+import cv2
+from matplotlib import pyplot as plt
+
+MIN_MATCH_COUNT = 100
+
+img1 = cv2.imread('pic1.jpg',0)          # queryImage
+img2 = cv2.imread('pic2.png',0) # trainImage
+
+# Initiate SIFT detector
+sift = cv2.xfeatures2d.SIFT_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks = 50)
+
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+
+# store all the good matches as per Lowe's ratio test.
+good = []
+for m,n in matches:
+    if m.distance < 0.7*n.distance:
+        good.append(m)
+
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+    matchesMask = mask.ravel().tolist()
+
+    h,w = img1.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+
+    img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+else:
+    #print ("Not enough matches are found " + str(len(good), str(MIN_MATCH_COUNT)))
+    matchesMask = None
+
+draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
+
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+
+plt.imshow(img3, 'gray'),plt.show()
