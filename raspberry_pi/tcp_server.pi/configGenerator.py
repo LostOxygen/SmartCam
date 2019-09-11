9570fe19c9718b948af944f650ee55e8409d6550
jonathan <jonathan.evertz@gmail.com>
2019-09-11 12:13:16 +0200

    old conversion

--- raspberry_pi/tcp_server.pi/configGenerator.py
+++ raspberry_pi/tcp_server.pi/configGenerator.py
@@ -58,59 +58,34 @@ class Config:
 
         for cnt in contours:
             area = cv2.contourArea(cnt)
-            rect = cv2.minAreaRect(cnt)
-            box = cv2.boxPoints(rect)
-            box = np.int0(box)
+            #if area > 0: #an die geforderte Größe anpassen
 
-            gray = np.float32(gray)
-            corners = cv2.goodFeaturesToTrack(gray, 20, 0.11, 100)
-            corners = np.int0(corners)
-
-#------------------- Quadrat aufspannen ----------------------
-            dist = [] #zum sortieren der Distanzen
-            ecken = [] #die entgültigen Ecken
-            punktedict = {
-                "a" : (0,0),
-                "b" : (0,0),
-                "c" : (0,0),
-                "d" : (0,0)
-            }
-
-            punkte = []
-            for corner in corners:
-                x,y = corner.ravel()
-                punkte.append((x,y))
-                cv2.circle(img, (x, y), 2, (255,255,0), 2)
-
-            punktedict["a"] = punkte[0] #erster Punkt ist Punkt a
-            punktedict["b"] = punkte[1] #erster Punkt ist Punkt a
-            punktedict["c"] = punkte[2] #erster Punkt ist Punkt a
-            punktedict["d"] = punkte[3] #erster Punkt ist Punkt a
-
-            dist_ab = math.sqrt((punkte[0][0] -  punkte[1][0])**2 + (punkte[0][1] - punkte[1][1])**2)
-            dist_ac = math.sqrt((punkte[0][0] -  punkte[2][0])**2 + (punkte[0][1] - punkte[2][1])**2)
-            dist_ad = math.sqrt((punkte[0][0] -  punkte[3][0])**2 + (punkte[0][1] - punkte[3][1])**2)
-
-            dist.append(((punkte[1][0], punkte[1][1]), dist_ab))
-            dist.append(((punkte[2][0], punkte[2][1]), dist_ac))
-            dist.append(((punkte[3][0], punkte[3][1]), dist_ad))
-
-            dist.sort(key=lambda elem: elem[1]) #sortieren
-
-            #von A zu den beiden nächsten Punkten zeichnen
-            cv2.line(img, punktedict["a"], dist[0][0], (255,255,0), 2)
-            cv2.line(img, punktedict["a"], dist[1][0], (255,255,0), 2)
-            #vom letzten Punkt zu den beiden die nicht A sind zeichnen
-            cv2.line(img, dist[2][0], dist[0][0], (255,255,0), 2)
-            cv2.line(img, dist[2][0], dist[1][0], (255,255,0), 2)
+            approx = cv2.approxPolyDP(cnt, 0.02*cv2.arcLength(cnt, True), True)
 
             try:
-                #conversion erstellen
-                edge_x1 = dist[0][1]
-                edge_x2 = dist[1][1]
-                edge_y1 = dist[0][1]
-                edge_y2 = dist[1][1]
+                Config.createCSV(approx, "approx")
+            except IndexError as e:
+                print("Nicht genug Konturen für <approx> gefunden.")
+                print(e)
 
+            x_values = [] #Listen für x und y werte um die passenden rauszusuchen
+            y_values = []
+
+            for i in approx:
+                x_values.append(i[0][0])
+                y_values.append(i[0][1])
+
+            upperLeft = (min(x_values), min(y_values))
+            upperRight = (max(x_values), min(y_values))
+            lowerLeft = (min(x_values), max(y_values))
+            lowerRight = (max(x_values), max(y_values))
+
+            edge_x1 = math.sqrt((upperLeft[0] - upperRight[0])**2 + (upperLeft[1] - upperRight[1])**2)
+            edge_x2 = math.sqrt((lowerLeft[0] - lowerRight[0])**2 + (lowerLeft[1] - lowerRight[1])**2)
+            edge_y1 = math.sqrt((upperLeft[0] - lowerLeft[0])**2 + (upperLeft[1] - lowerLeft[1])**2)
+            edge_y2 = math.sqrt((upperRight[0] - lowerRight[0])**2 + (upperRight[1] - lowerRight[1])**2)
+
+            try:
                 edge_x1_mm = (edgelength / edge_x1) #conversion in mm per pixel
                 edge_x2_mm = (edgelength / edge_x2)
                 edge_y1_mm = (edgelength / edge_y1)
@@ -120,15 +95,81 @@ class Config:
                 conversion_mm_per_pixel = mean
 
                 edges = (edge_x1, edge_x2, edge_y1, edge_y2)
-
             except:
                 print("Fehler: womöglich wurden keine Kanten gefunden .. ")
                 conversion_mm_per_pixel = 1
                 edges = (1, 1, 1, 1)
+            #area = cv2.contourArea(cnt)
+            #rect = cv2.minAreaRect(cnt)
+            #box = cv2.boxPoints(rect)
+            #box = np.int0(box)
+
+            #gray = np.float32(gray)
+            #corners = cv2.goodFeaturesToTrack(gray, 20, 0.11, 100)
+            #corners = np.int0(corners)
+
+#------------------- Quadrat aufspannen ----------------------
+            #dist = [] #zum sortieren der Distanzen
+            #ecken = [] #die entgültigen Ecken
+            #punktedict = {
+            #    "a" : (0,0),
+            #    "b" : (0,0),
+            #    "c" : (0,0),
+            #    "d" : (0,0)
+            #}
+
+            #punkte = []
+            #for corner in corners:
+            #    x,y = corner.ravel()
+            #    punkte.append((x,y))
+            #    cv2.circle(img, (x, y), 2, (255,255,0), 2)
+
+            #punktedict["a"] = punkte[0] #erster Punkt ist Punkt a
+            #punktedict["b"] = punkte[1] #erster Punkt ist Punkt a
+            #punktedict["c"] = punkte[2] #erster Punkt ist Punkt a
+            #punktedict["d"] = punkte[3] #erster Punkt ist Punkt a
+
+            #dist_ab = math.sqrt((punkte[0][0] -  punkte[1][0])**2 + (punkte[0][1] - punkte[1][1])**2)
+            #dist_ac = math.sqrt((punkte[0][0] -  punkte[2][0])**2 + (punkte[0][1] - punkte[2][1])**2)
+            #dist_ad = math.sqrt((punkte[0][0] -  punkte[3][0])**2 + (punkte[0][1] - punkte[3][1])**2)
+
+            #dist.append(((punkte[1][0], punkte[1][1]), dist_ab))
+            #dist.append(((punkte[2][0], punkte[2][1]), dist_ac))
+            #dist.append(((punkte[3][0], punkte[3][1]), dist_ad))
+
+            #dist.sort(key=lambda elem: elem[1]) #sortieren
+            #von A zu den beiden nächsten Punkten zeichnen
+            #cv2.line(img, punktedict["a"], dist[0][0], (255,255,0), 2)
+            #cv2.line(img, punktedict["a"], dist[1][0], (255,255,0), 2)
+            #vom letzten Punkt zu den beiden die nicht A sind zeichnen
+            #cv2.line(img, dist[2][0], dist[0][0], (255,255,0), 2)
+            #cv2.line(img, dist[2][0], dist[1][0], (255,255,0), 2)
+
+            #try:
+                #conversion erstellen
+            #    edge_x1 = dist[0][1]
+            #    edge_x2 = dist[1][1]
+            #    edge_y1 = dist[0][1]
+            #    edge_y2 = dist[1][1]
+
+            #    edge_x1_mm = (edgelength / edge_x1) #conversion in mm per pixel
+            #    edge_x2_mm = (edgelength / edge_x2)
+            #    edge_y1_mm = (edgelength / edge_y1)
+            #    edge_y2_mm = (edgelength / edge_y2)
+
+            #    mean = (edge_x1_mm + edge_x2_mm + edge_y1_mm + edge_y2_mm) / 4
+            #    conversion_mm_per_pixel = mean
+
+            #    edges = (edge_x1, edge_x2, edge_y1, edge_y2)
+
+            #except:
+            #    print("Fehler: womöglich wurden keine Kanten gefunden .. ")
+            #    conversion_mm_per_pixel = 1
+            #    edges = (1, 1, 1, 1)
 
             Config.writeConfig(img_order, height, conversion_mm_per_pixel, edges) #writes the .ini file
 
-            img = Config.visualization(img, punktedict, edges) #writes text and draws the rectangel into the img
+            img = Config.visualization(img, points, edges) #writes text and draws the rectangel into the img
 
         Config.saveImg(img_order, img) #saves img at the end
 
@@ -137,11 +178,11 @@ class Config:
         configpar = configparser.ConfigParser()
 
         if str(img_order) == "1":
-            configpar['tcp'] = {'host' : '192.168.8.6x',
+            configpar['tcp'] = {'host' : '192.168.8.60',
                              'port' : '65432'}
-            configpar['web'] = {'web_host' : '134.147.234.23x',
+            configpar['web'] = {'web_host' : '134.147.234.232',
                              'web_port' : '80'}
-            configpar['conversion'] = {'distanceToObject1' : height,
+            configpar['conversion'] = {'distanceToObject1' : str(height),
                                     'mm_per_pixel1' : conversion_mm_per_pixel,
                                     'distanceToObject2' : 0,
                                     'scalingFactor' : 1}
@@ -191,11 +232,11 @@ class Config:
         return timestamp
 
 # ----------------------------------- Visual -----------------------
-    def visualization(img, punktedict, edges):
-        cv2.putText(img, "A" , punktedict["a"], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 1, cv2.LINE_AA, 0)
-        cv2.putText(img, "B" , punktedict["b"], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 1, cv2.LINE_AA, 0)
-        cv2.putText(img, "C" , punktedict["c"], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 1, cv2.LINE_AA, 0)
-        cv2.putText(img, "D" , punktedict["d"], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 1, cv2.LINE_AA, 0)
+    def visualization(img, points, edges):
+        #cv2.putText(img, "A" , punktedict["a"], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 1, cv2.LINE_AA, 0)
+        #cv2.putText(img, "B" , punktedict["b"], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 1, cv2.LINE_AA, 0)
+        #cv2.putText(img, "C" , punktedict["c"], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 1, cv2.LINE_AA, 0)
+        #cv2.putText(img, "D" , punktedict["d"], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 1, cv2.LINE_AA, 0)
 
         cv2.putText(img, "edge_x1 = " + str(edges[0]), (100,100), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 1, cv2.LINE_AA, 0)
         cv2.putText(img, "edge_y1 = " + str(edges[1]) , (100,150), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 1, cv2.LINE_AA, 0)
