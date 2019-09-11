4f74f51ef621c7b3af6e54a99cea2f0d0cf3f3fb
jonathan <jonathan.evertz@gmail.com>
2019-09-11 11:51:05 +0200

    .

--- raspberry_pi/tcp_server.pi/server.py
+++ raspberry_pi/tcp_server.pi/server.py
@@ -45,6 +45,8 @@ config_test = True #zum Abfragen der Config
 
 #---------------------------- Beginn Server ------------------------------------------------------
 def main():
+    print(HOST)
+    print(PORT)
     print("Befehle: \n EX : beendet Server und Verbindung \n GO : liefert Offset zurueck \n CFGx : konfiguriert die Kreiserkennung \n IM : erstellt ein einfaches Bild \n CV : erstellt ein Bild mit erkannten Kreisen \n FX123: setzt LED-Ring auf Farbwert 123,123,123 \n KOx: liefert Kabeloffset zurück \n")
     #strip.begin()
     print("Hostet Server auf: " + get_ip("wlan0") + " auf Port: " + str(PORT))
