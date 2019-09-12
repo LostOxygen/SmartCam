8a8e6a8bb150cb02dc3685c23dfa6a166241cfcf
jonathan <jonathan.evertz@gmail.com>
2019-09-12 09:42:19 +0200

    thread time

--- raspberry_pi/tcp_server.pi/camera_pi.py
+++ raspberry_pi/tcp_server.pi/camera_pi.py
@@ -83,6 +83,6 @@ class Camera(object):
 
                 # if there hasn't been any clients asking for frames in
                 # the last 10 seconds stop the thread
-                if time.time() - cls.last_access > 10:
+                if time.time() - cls.last_access > 100:
                     break
         cls.thread = None
