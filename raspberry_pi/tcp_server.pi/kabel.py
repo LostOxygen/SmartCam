4d70cecab710361f677c2cc62c7dab7064af3b99
jonathan <jonathan.evertz@gmail.com>
2019-09-13 14:52:58 +0200

    update

--- raspberry_pi/tcp_server.pi/kabel.py
+++ raspberry_pi/tcp_server.pi/kabel.py
@@ -106,7 +106,7 @@ class Kabel():
         print("extLeft: " + str(extLeft))
 
         g_height, g_width = gray.shape[:2]
-        g_mittelpunkt = (int(g_width/2), int(g_height/2))
+        g_mittelpunkt = (g_width, int(g_height/2))
 
         cv2.circle(gray, extLeft, 4, (255, 255, 255), 4) #zeichnet punkt ganz links
         cv2.line(gray, extLeft, (extLeft[0], int(g_height/2)), (255,255,255), 2) #zeichnet linie von punkt nach oben
@@ -115,39 +115,21 @@ class Kabel():
         cv2.line(gray, g_mittelpunkt, (extLeft[0], int(g_height/2)), (255,255,255), 2)
         cv2.line(gray, g_mittelpunkt, extLeft, (255,255,255), 2)
 
-        dy = math.sqrt((extLeft[0] - extLeft[0])**2 + (extLeft[1] - mittelpunkt[1])**2)
-        dx = math.sqrt((mittelpunkt[0] - extLeft[1])**2 + (mittelpunkt[1] - mittelpunkt[1])**2)
+        dist_y = g_mittelpunkt[1] - extLeft[1]
+        dist_x = g_mittelpunkt[0] - extLeft[0]
 
-        corners = cv2.goodFeaturesToTrack(gray, maxCorners, qualityLevel, minDistance)
-        #corners = np.int0(corners)
+        min_xy = (int(mittelpunkt[0] - dx), int(mittelpunk[1] - dy))
 
-        min_xy = (1000,1000) #temp Var zum finden von punkt ganz oben_links
-        if corners is not None:
-            for i in corners:
-                x,y = i.ravel()
-                if x <= 1100: #zeichnet nur Relevante Punkte
-                    cv2.circle(img, (int(x + oben_links[0]), int(y + oben_links[1])), 2, (0,0,255), 2)
-                if x < min_xy[0]: #guckt nach kleinstem x wert
-                #    min_xy = (int(x + oben_links[0]), int(y + oben_links[0]))
-                    if x > extLeft[0]:
-                       min_xy = (int(extLeft[0]+oben_links[0]), int(extLeft[1]+oben_links[1]))
-                    else:
-                        min_xy = (int(x + oben_links[0]), int(y + oben_links[0]))
-        print("groß: " + str(min_xy))
-
-        #berechnet Distanz von Höhe und Länge des Kabels
-        dist_y = math.sqrt((min_xy[0] - min_xy[0])**2 + (min_xy[1] - mittelpunkt[1])**2)
-        dist_x = math.sqrt((mittelpunkt[0] - min_xy[1])**2 + (mittelpunkt[1] - mittelpunkt[1])**2)
-        dist_y = int(dist_y * umrechnung_pixel_mm)
-        dist_x = int(dist_x * umrechnung_pixel_mm)
+        dist_y_mm = dist_y * umrechnung_pixel_mm
+        dist_x_mm = dist_x * umrechnung_pixel_mm
 
     #----------- optische Ausgabe --------------------------
         #Konturen zeichnen
         #cv2.drawContours(img, contours[0], -1, (0,255,0), 3)
 
-        cv2.circle(img, (int(min_xy[0] - dx), int(min_xy[1] - dy)), 4, (255, 255, 0), 6) #zeichnet punkt ganz links
+        cv2.circle(img, (int(mittelpunkt[0] - dx), int(mittelpunk[1] - dy)), 4, (255, 255, 0), 6) #zeichnet punkt ganz links
 
-        cv2.circle(img, min_xy, 4, (255, 255, 255), 4) #zeichnet punkt ganz links
+        #cv2.circle(img, min_xy, 4, (255, 255, 255), 4) #zeichnet punkt ganz links
         cv2.line(img, min_xy, (min_xy[0], int(height/2)), (255,255,255), 2) #zeichnet linie von punkt nach oben
         #zeichnet Mittelpunkt und Linie nach links
         cv2.circle(img, mittelpunkt, 2, (255,255,255), 2)
@@ -163,11 +145,11 @@ class Kabel():
 
         #Todo Sekunde programmieren
         timestamp = "" + str(imgDate) + "." + str(imgMonth) + "." + str(imgYear) + " " + str(imgHour) + ":" + str(imgMins)
-        cv2.putText(img, timestamp + " | " + "" + str(round(dist_x, 2)) + " mm | " + "" + str(round(dist_y, 2)) + " mm ", (20,1040), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,0), 2, cv2.LINE_AA, 0)
+        cv2.putText(img, timestamp + " | " + "x: " + str(round(dist_x_mm, 2)) + " mm | " + "y: " + str(round(dist_y_mm, 2)) + " mm ", (20,1060), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,0), 2, cv2.LINE_AA, 0)
     #-------------------------------------------------------
         #Umrechnung per Config in mm
         print("Distanz_Y: " + str(dist_y))
-        print("umgerechnet: " + str(round(umrechnung_pixel_mm * dist_y)) + "mm")
+        print("umgerechnet: " + str(round(dist_y_mm), 2) + "mm")
 
         if str(bild_num) == "1":
             print("Speichert kabel1.jpg in /home/pi/RoboSchalt/raspberry_pi/bilder/")
