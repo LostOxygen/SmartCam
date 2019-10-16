55252cedeceb5c99a2d1d64ecf8159cb95d8451e
jonathan <jonathan.evertz@gmail.com>
2019-10-16 11:00:49 +0200

    added layout problem solver

---
+++ sonstiges/init_layout.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+import sys
+import os
+import numpy as np
+
+def auxiliary(A):
+    _A = []
+
+    for row in range(len(A)):
+
+
+        for element in range(len(A[row])):
+            if A[row][element] == 1:
+
+
+        print(distributeTo)
+
+
+
+
+    return matrix
+
+def main(argv):
+    A = [[0,1,1,1,0,0],
+         [0,0,0,0,1,0],
+         [0,0,0,0,1,0],
+         [0,0,0,0,0,1],
+         [0,0,0,0,0,1],
+         [0,0,0,0,0,0]]
+
+    A = auxiliary(A)
+
+    #print(A)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
